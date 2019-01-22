18d4fd794f34c329ca003841646dc866dd66e8c3
Sandeep Maity <sandeepmaity09@gmail.com>
2019-01-22 23:15:12 +0530

    Testing Exception

--- tests/lib2.spec.js
+++ tests/lib2.spec.js
@@ -5,13 +5,13 @@ describe('absolute', () => {
         const result = lib.absolute(1);
         expect(result).toBe(1);
     })
-    
-    
+
+
     it('absolute - should return a positive number if input is negative', () => {
         const result = lib.absolute(-1);
         expect(result).toBe(1);
     })
-    
+
     it('absolute - should return a 0 if input is 0', () => {
         const result = lib.absolute(0);
         expect(result).toBe(0);
@@ -34,48 +34,76 @@ describe('greet', () => {
 })
 
 describe('getCurriencies', () => {
-       it('should return supported curriencies', () => {
-           const result = lib.getCurrencies();
-           
-           // Too General
-           expect(result).toBeDefined();
+    it('should return supported curriencies', () => {
+        const result = lib.getCurrencies();
 
-           // Too General
-           expect(result).not.toBeNull();
+        // Too General
+        expect(result).toBeDefined();
 
-           // Too Specific
-           expect(result[0]).toBe('USD');
-           expect(result[1]).toBe('AUD');
-           expect(result[2]).toBe('EUR');
+        // Too General
+        expect(result).not.toBeNull();
 
-           // Don't look for specific position in the array
+        // Too Specific
+        expect(result[0]).toBe('USD');
+        expect(result[1]).toBe('AUD');
+        expect(result[2]).toBe('EUR');
 
-
-           // Too Specific
-           expect(result.length).toBe(3);
-
-           // Will Break if add more elements in array
+        // Don't look for specific position in the array
 
 
+        // Too Specific
+        expect(result.length).toBe(3);
 
-           // PROPER WAY
-           expect(result).toContain('USD');
-           expect(result).toContain('AUD');
-           expect(result).toContain('EUR');           
+        // Will Break if add more elements in array
 
 
-           // Ideal Way : Balanced Way
-           expect(result).toEqual(expect.arrayContaining(['EUR', 'USD', 'AUD']))
-       }) 
+
+        // PROPER WAY
+        expect(result).toContain('USD');
+        expect(result).toContain('AUD');
+        expect(result).toContain('EUR');
+
+
+        // Ideal Way : Balanced Way
+        expect(result).toEqual(expect.arrayContaining(['EUR', 'USD', 'AUD']))
+    })
 })
 
 describe('getProduct', () => {
     it('should return the product with the given id', () => {
         const result = lib.getProduct(1);
         // expect(result).toBe({id: 1, price: 10}); // toBe Matches refreneces
-        expect(result).toEqual({id: 1, price: 10});  // check exect same even protos
-        expect(result).toMatchObject({id:1, price:10});
+        expect(result).toEqual({ id: 1, price: 10 });  // check exect same even protos
+        expect(result).toMatchObject({ id: 1, price: 10 });
 
         expect(result).toHaveProperty('id', 1);  // type of data for key's value matter
+    })
+})
+
+describe('registerUser', () => {
+    it('should throw if username is falsy', () => {
+        // Falsy Values :- undefined, null, '', 0, false, "", NaN
+
+        // const result = lib.registerUser(null);
+        // expect(result).toThrow();
+
+        // We can't expect a result here cause this will throw error
+
+        // expect(() => { lib.registerUser(null) }).toThrow();
+    
+        // sometimes the test functions maybe few lines long in that case 
+        // you can use parameterized test. NOT SUPPORTED BY JEST
+
+        // Alternative Method
+        const args = [null, undefined, NaN, '', 0, false];
+        args.forEach(a => {
+            expect(() => { lib.registerUser(a) }).toThrow();            
+        })
+    });
+
+    it('should return a user object if valid username is passed', () => {
+        const result = lib.registerUser("Sandeep");
+        expect(result).toMatchObject({username: "Sandeep"})
+        expect(result.id).toBeGreaterThan(0);
     })
 })
